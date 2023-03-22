8637e4a1eba1b1f478b53bb5ec254bbe57794156
Creativeme_wedchayun Sorut <creativeme_wedchayun.sorut@krungthai.com>
2023-03-22 11:06:04 +0700

    fix: s

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
       </Head>
+
       <React.StrictMode>
         <Component {...pageProps} />
       </React.StrictMode>
